51d443bbc5709cee16c3c17edf78137306d73595
Mario Lopez <mariodolt@marios-mbp.lan>
2023-05-31 23:12:39 -0700

    basic fetch

---
+++ src/consts.ts
@@ -0,0 +1,3 @@
+export const baseApiUrl =
+  "https://ae3t7l1i79.execute-api.us-east-1.amazonaws.com/";
+// if real then I would have some func to return the right api for dev, test, prod envs
---
+++ src/hooks/useBundleQuery/types.ts
@@ -0,0 +1,7 @@
+export type BundleType = {
+  handle: string;
+  title: string;
+  products_included: string[];
+  price: number;
+  imageSrc: string;
+};
