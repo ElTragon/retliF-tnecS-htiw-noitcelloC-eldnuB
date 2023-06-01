f3f88073f893d7b55dbbda7a1a4dbbc8d82e992e
Mario Lopez <mariodolt@marios-mbp.lan>
2023-06-01 09:03:50 -0700

    done?

--- src/utils/Seo.tsx
+++ src/utils/Seo.tsx
@@ -8,6 +8,16 @@ export default function Seo() {
         name="description"
         content="Bundle Collection with Scent Filter by Mario"
       />
+      <meta
+        name="image"
+        property="og:image"
+        content="https://cdn.shopify.com/s/files/1/0275/7784/3817/files/DRS_horizontal_fullcolor.svg?v=1615332033 "
+      />
+
+      <meta
+        name="twitter:image"
+        content="https://cdn.shopify.com/s/files/1/0275/7784/3817/files/DRS_horizontal_fullcolor.svg?v=1615332033 "
+      />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
