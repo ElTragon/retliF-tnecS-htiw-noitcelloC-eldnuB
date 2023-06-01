695320f5630baf53a7286b39296c6faf0c3e3032
Mario Lopez <mariodolt@marios-mbp.lan>
2023-05-31 23:16:52 -0700

    wording

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
 export const baseApiUrl =
   "https://ae3t7l1i79.execute-api.us-east-1.amazonaws.com/";
-// if real then I would have some func to return the right api for dev, test, prod envs
+// in the real world, I would have some func to return the right url for dev, test, prod envs
